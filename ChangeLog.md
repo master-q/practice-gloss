d1208ec8cf3adbac111c7b045f63540ee254cca9
Kiwamu Okabe <kiwamu-okabe@shinkawa.com>
2018-04-13 11:33:19 +0900

    Start from import:
    git@github.com:master-q/masterq-docs.git/presentations/20120527_emacs-gloss

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for practice-gloss
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,34 @@
+module Main where
+import System.Environment
+import qualified MainGloss1
+import qualified MainGloss2
+import qualified MainGloss3
+import qualified MyGloss1
+import qualified MyGloss2
+import qualified MyGloss3
+
+usage :: IO ()
+usage = do
+  f "Usage: practice-gloss-exe FOLLOWING_ARGS"
+  f "  MainGloss1"
+  f "  MainGloss2"
+  f "  MainGloss3"
+  f "  MyGloss1"
+  f "  MyGloss2"
+  f "  MyGloss3"
+  where
+    f = putStrLn
+
+main :: IO ()
+main = do
+  a <- getArgs
+  f a
+  where
+    f :: [String] -> IO ()
+    f ["MainGloss1"] = MainGloss1.main
+    f ["MainGloss2"] = MainGloss2.main
+    f ["MainGloss3"] = MainGloss3.main
+    f ["MyGloss1"]   = MyGloss1.main
+    f ["MyGloss2"]   = MyGloss2.main
+    f ["MyGloss3"]   = MyGloss3.main
+    f _                = usage
